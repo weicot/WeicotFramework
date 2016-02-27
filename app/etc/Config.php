c2143dcbcca5562e7039e6e834f69a2850549b9d
WeiCot <1050653098@qq.com>
2016-02-28 00:25:05 +0800

    Update Config.php

--- app/etc/Config.php
+++ app/etc/Config.php
@@ -7,5 +7,5 @@ define('DB_DATABASE',"");
 define('DB_HOST','127.0.0.1');
 define('DB_NAME','weicotsys');
 define('DB_USER','root');
-define('DB_PASSWORD','1.24mll');
+define('DB_PASSWORD','xxxxx');
 #没有找到页面的返回页面 Base/404.php
