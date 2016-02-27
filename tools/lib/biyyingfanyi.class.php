461c11adc1577497c2f6627f71fae0d67f53da09
WeiCot <1050653098@qq.com>
2016-02-27 09:04:26 +0800

    Update biyyingfanyi.class.php

--- tools/lib/biyyingfanyi.class.php
+++ tools/lib/biyyingfanyi.class.php
@@ -186,9 +186,9 @@ try {
 
 
 
-$clientID       = "ajiang";
+$clientID       = "xxxxx";
     //Client Secret key of the application.
-    $clientSecret = "JRxH3kuxeWmOl7Jyn0BUEhj7tkvXCYNv89JhKZp6X7M=";
+    $clientSecret = "xxxxxx";
     //OAuth Url.
     $authUrl      = "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13/";
     //Application Scope Url
